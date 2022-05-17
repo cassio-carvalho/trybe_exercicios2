f8b5b7597b5d66d26bb6efde6cbb0bd009c09ad6
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-05-17 18:09:38 -0300

    Exercicio 1 completo

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script2.js
@@ -0,0 +1,7 @@
+const acordar = () => console.log('Acordando!!');
+const cafe = () => console.log('Bora tomar café!!');
+const dormir = () => console.log('Partiu dormir!!');
+
+const doingThings = (thing) => thing;
+
+doingThings(cafe());
--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,1 +1,18 @@
 // Exercicio 1
+
+const listaEmailsTrybe = { nomeCompleto: '', email: '' }
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+const criaEmail = (nome) => {
+  const email = `${nome.replace(' ', '_').toLowerCase()}@trybe.com`;
+  return { nomeCompleto: nome, email: email};
+}
+console.log(newEmployees(criaEmail));
