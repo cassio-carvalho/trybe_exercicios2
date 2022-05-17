80f75cfc9e31480f59211631d86814b6e02aad23
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-05-17 18:26:33 -0300

    Exercicio 2 completo

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,18 +1,32 @@
 // Exercicio 1
 
-const listaEmailsTrybe = { nomeCompleto: '', email: '' }
+// const listaEmailsTrybe = { nomeCompleto: '', email: '' }
 
-const newEmployees = (callback) => {
-  const employees = {
-    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-  }
-  return employees;
-};
+// const newEmployees = (callback) => {
+//   const employees = {
+//     id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+//     id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+//     id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+//   }
+//   return employees;
+// };
 
-const criaEmail = (nome) => {
-  const email = `${nome.replace(' ', '_').toLowerCase()}@trybe.com`;
-  return { nomeCompleto: nome, email: email};
+// const criaEmail = (nome) => {
+//   const email = `${nome.replace(' ', '_').toLowerCase()}@trybe.com`;
+//   return { nomeCompleto: nome, email: email};
+// }
+// console.log(newEmployees(criaEmail));
+
+// Exercicio 2
+
+const geradorNumero = () => {
+  return Math.floor((Math.random() * 5) +1);
 }
-console.log(newEmployees(criaEmail));
+
+const resultadoSorteio = (aposta, geradorNumero) => {
+  if (aposta === geradorNumero) {
+    return `Número sorteado: ${geradorNumero}; Parabéns você ganhou!`;
+  }
+  return `Número sorteado: ${geradorNumero}; Tente novamente.`
+}
+console.log(resultadoSorteio(2, geradorNumero()));
